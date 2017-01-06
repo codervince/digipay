9a4e13fffab4fad2fb655918c29b782cc08ffb04
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-06 19:43:48 +0200

    small fix for admin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -4,5 +4,6 @@ from .models import Transaction
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('email', 'hash', 'amount_usd', 'amount_btc',)
+    list_display = ('email', 'hash', 'project_code', 'amount_usd',
+                    'amount_btc', 'to_address',)
     list_filter = ('created_at', 'updated_at')
