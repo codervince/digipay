35e33be9f0121117e0ec1c7965ba2138b24aa0d7
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-06 19:43:10 +0200

    changes on the model

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -7,9 +7,10 @@ class Transaction(BaseModel):
    """
    email = models.EmailField(null=True)
    hash = models.UUIDField(null=True)
+   project_code = models.CharField(max_length=255, null=True)
    amount_usd = models.DecimalField(max_digits=10, decimal_places=2)
    amount_btc = models.DecimalField(max_digits=10, decimal_places=2)
-   receiving_address = models.TextField(null=True, blank=True)
+   to_address = models.TextField(null=True, blank=True)
 
    class Meta:
        db_table = 'transactions'
