861060e011142dc1d778d86af87ef909e000892a
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-06 19:45:39 +0200

    work on models changes for to_address and project_code

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -7,10 +7,10 @@ class Transaction(BaseModel):
    """
    email = models.EmailField(null=True)
    hash = models.UUIDField(null=True)
-   project_code = models.CharField(max_length=255, null=True)
+   project_code = models.CharField(max_length=34, null=True)
    amount_usd = models.DecimalField(max_digits=10, decimal_places=2)
    amount_btc = models.DecimalField(max_digits=10, decimal_places=2)
-   to_address = models.TextField(null=True, blank=True)
+   to_address = models.CharField(max_length=34, null=True)
 
    class Meta:
        db_table = 'transactions'
