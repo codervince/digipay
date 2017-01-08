a7b74a6e8fc4f197f7031ba0585dd35d9fd4a9f7
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-08 12:49:53 +0200

    payments: show status in admin transaction list view

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -4,7 +4,7 @@ from .models import Transaction
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('email', 'project_code', 'amount_usd',
+    list_display = ('email', 'status', 'project_code', 'amount_usd',
                     'amount_btc', 'from_address', 'to_address',)
     list_filter = ('status', 'created_at', 'updated_at')
     fields = (
