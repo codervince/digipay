4ceb82f2b109368bb605e578aea3fbd54ec2db36
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-08 11:38:22 +0200

    add fields to payments/admin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -7,6 +7,9 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ('email', 'project_code', 'amount_usd',
                     'amount_btc', 'from_address', 'to_address',)
     list_filter = ('status', 'created_at', 'updated_at')
+    fields = (
+        'site', 'project', 'email', 'from_address', 'to_address', 'amount_usd'
+    )
 
     def project_code(self, obj):
         return obj.project.code.hex
