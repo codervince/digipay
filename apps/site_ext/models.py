e5003f9d474ac4a9d193f09f3d3baccb3e719350
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-14 02:51:26 +0200

    site_ext: fix http://sentry.inhacking.com/sentry/btc-gateway-stage/issues/340/

--- apps/site_ext/models.py
+++ apps/site_ext/models.py
@@ -1,6 +1,5 @@
 import uuid
 from django.db import models
-from django.db.models import signals
 from django.contrib.sites.models import Site
 from core.models import BaseModel
 from django.utils.translation import ugettext_lazy as _
@@ -12,12 +11,3 @@ class SiteExt(BaseModel):
     token = models.UUIDField(default=uuid.uuid4, editable=False, null=True)
     api_key = models.CharField(max_length=255, blank=True, null=True)
     callback_url = models.URLField(blank=True, null=True)
-
-
-def create_ext(sender, instance, created, **kwargs):
-    """Create SiteExt for every new Site."""
-    if created:
-        SiteExt.objects.create(site=instance)
-
-signals.post_save.connect(create_ext, sender=Site, weak=False,
-                          dispatch_uid='models.create_ext')
