47f0195c74007913c5885c2ca7f9fba8351f309b
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-18 01:00:17 +0200

    add txid and amount_paid columns

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -5,7 +5,7 @@ from .models import Transaction
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('email', 'status', 'project_code', 'amount_usd',
-                    'amount_btc', 'to_address')
+                    'amount_btc', 'to_address', 'txid', 'amount_paid')
     list_filter = ('status', 'created_at', 'updated_at')
     fields = (
         'site', 'email', 'to_address', 'amount_usd'
