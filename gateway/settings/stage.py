20d839ef87fd8fd8f114502082c0f52164ed572c
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-20 02:52:29 +0200

    add mail settings to stage

--- gateway/settings/stage.py
+++ gateway/settings/stage.py
@@ -15,3 +15,9 @@ RAVEN_CONFIG = {
 INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
 )
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.getenv('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
