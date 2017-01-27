98b5e9c47c861ecc9e6b9c1d83ae05c26e8adc63
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-01-27 02:09:07 +0200

    put error message there

--- apps/api/views.py
+++ apps/api/views.py
@@ -287,7 +287,9 @@ class TransactionsLatestAPIView(View):
     def get(self, request, *args, **kwargs):
         secret = request.GET.get('secret')
         if any([not secret, secret != settings.SECRET]):
-            return JsonResponse(data={}, status=400)
+            return JsonResponse(data={
+                "error": _("Invalid secret key")
+            }, status=400)
 
         gte = timezone.now() - datetime.timedelta(
             minutes=settings.LATEST_TX_NU)
